8ea7b0affc8f78626548694458c08ed76fbd6faa
sgiehl <stefan@piwik.org>
2016-12-30 13:39:46 +0100

    updates mocha

--- tests/UI/config.dist.js
+++ tests/UI/config.dist.js
@@ -30,7 +30,7 @@ exports.php = 'php';
 /**
  * The folder in tests/lib that holds mocha.
  */
-exports.mocha = 'mocha-3.1.0';
+exports.mocha = 'mocha-3.2.0';
 
 /**
  * The folder in tests/lib that holds chai.
